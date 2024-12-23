72efb638f2c0f66db5224060603f82a3deed1a75
alessandrochetta <alessandrochetta@gmail.com>
2024-12-22 23:32:52 -0600

    Build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/url-decoder",
   output: "export",
   reactStrictMode: true,
+  distDir: 'docs'
 };
 
 export default nextConfig;
